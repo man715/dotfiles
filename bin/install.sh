e2416972bcede150fa1c3c797d846a7f864e78ee
Arron Patton <man715@protonmail.com>
2022-05-16 10:42:55 -0600

    remove sudo from base

--- bin/install.sh
+++ bin/install.sh
@@ -61,10 +61,10 @@ base() {
 		passwd $TARGET_USER
 	fi
 	# Prep Docker installation
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 		
 	apt update || true
 	apt -y upgrade
