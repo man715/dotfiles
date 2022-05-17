40a56481e8b3890ba900b5033d9ac5c2b3f0c553
Arron Patton <man715@protonmail.com>
2022-05-17 06:00:39 -0600

    .

--- bin/install.sh
+++ bin/install.sh
@@ -185,7 +185,7 @@ install_scripts() {
 		chown -R $TARGET_USER:$TARGET_USER /home/$TARGET_USER/dotfiles
     fi
 	
-    runuser -l $TARGET_USER 'cd $HOME/dotfiles && make scripts' 
+    runuser -l $TARGET_USER 'cd ${HOME}/dotfiles && make scripts' 
 	chown -R $TARGET_USER:$TARGET_USER /home/$TARGET_USER
 
 }
@@ -291,7 +291,7 @@ install_dotfiles() {
         git clone git@github.com:man715/dotfiles.git /home/$TARGET_USER/dotfiles
 		chown -R $TARGET_USER:$TARGET_USER /home/$TARGET_USER/dotfiles
     fi
-    runuser -l $TARGET_USER 'cd $HOME/dotfiles && make dotfiles'
+    runuser -l $TARGET_USER 'cd ${HOME}/dotfiles && make dotfiles'
 	chown -R $TARGET_USER:$TARGET_USER /home/$TARGET_USER
 }
 
