da2b6ea3adc19b4ef6915dee6107d9dcba655695
Arron Patton <man715@protonmail.com>
2022-05-17 07:32:25 -0600

    .

--- bin/install.sh
+++ bin/install.sh
@@ -78,10 +78,12 @@ base() {
 	echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt install -y \	
+		systemd-timesyncd
+		--no-install-recommends
 		
 	apt install -y \
 		ntp \
-		systemd-timesyncd
 		--no-install-recommends
 
 	apt update
