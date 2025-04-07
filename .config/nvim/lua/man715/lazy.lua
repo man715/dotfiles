ded07908b061c486b8c4ae35f513949f3e5323d5
apatton <apatton@mantis-main.local.dragonsploit.com>
2025-04-07 08:39:28 -0500

    changed themes

--- .config/nvim/lua/man715/lazy.lua
+++ .config/nvim/lua/man715/lazy.lua
@@ -587,35 +587,16 @@ require('lazy').setup({
   },
 
   {
-    'ellisonleao/gruvbox.nvim',
+    'Mofiqul/adwaita.nvim',
+    lazy = false,
     priority = 1000,
+
+    -- configure and set on startup
     config = function()
-      require('gruvbox').setup {
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = 'hard', -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      }
-      vim.o.background = 'light'
-      vim.cmd 'colorscheme gruvbox'
+      vim.g.adwaita_darker = false -- for darker version
+      vim.g.adwaita_disable_cursorline = false -- to disable cursorline
+      vim.g.adwaita_transparent = false -- makes the background transparent
+      vim.cmd 'colorscheme adwaita'
     end,
   },
 
